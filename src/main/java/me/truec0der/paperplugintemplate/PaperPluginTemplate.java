c80a1c00b14da3b3230071a9215b12548d1539ba
TRUEC0DER <tcoder090@gmail.com>
2025-03-17 06:11:42 +0300

    Init template commit

---
+++ src/main/java/me/truec0der/paperplugintemplate/PaperPluginTemplate.java
@@ -0,0 +1,13 @@
+package me.truec0der.paperplugintemplate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PaperPluginTemplate extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
